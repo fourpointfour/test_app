91f496125a6c5fd22d13f7dc67881273c50f6975
Vaibhav Kushwaha <vaibhav20092000@gmail.com>
2020-12-30 18:01:30 +0530

    modal bottom sheet and bottom sheet shown

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,34 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.lightBlueAccent,
-      child: Scaffold(),
+      child: Scaffold(
+        floatingActionButton: MyActionButton (),
+      ),
+    );
+  }
+}
+
+class MyActionButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      child: Icon(
+        Icons.add,
+        size: 30,
+      ),
+      onPressed: (){
+        /*
+          Modal bottom sheet doesn't allow the user to interact with the main app if the bottom sheet is displayed
+          but if you want to interact with the app as well as the bottom sheet, use showBottomSheet()
+         */
+        showModalBottomSheet(
+          context: context,
+          builder: (context) => Container(
+            color: Colors.red,
+            height: 200,
+          ),
+        );
+      },
     );
   }
 }
