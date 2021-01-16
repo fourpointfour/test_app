2e6f1142b1980ef368b8bf5be33fd2b1194da4b1
Vaibhav Kushwaha <vaibhav20092000@gmail.com>
2021-01-16 19:46:19 +0530

    implemented neumorphism UI on a box with a reddish color

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,51 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.lightBlueAccent,
-      child: Scaffold(),
+    // var pixelRatio = MediaQuery.of(context).devicePixelRatio;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Neumorphism UI'),
+        backgroundColor: Colors.blue,
+      ),
+      backgroundColor: Color(0xffdb3553),
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            height: 150,
+            width: 150,
+            decoration: BoxDecoration(
+              color: Color(0xffdb3553),
+              borderRadius: BorderRadius.all(Radius.circular(50)),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xffba2d47),
+                  offset: Offset(4, 4),
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                ),
+                BoxShadow(
+                  color: Color(0xfffc3d5f),
+                  offset: Offset(-4, -4),
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                ),
+              ]
+            ),
+            child: Center(
+              child: Text(
+                  'Hi there!',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
