a2d43403b73141cef9a0985bcbfc385b268a3268
Vaibhav Kushwaha <vaibhav20092000@gmail.com>
2021-02-05 19:39:38 +0530

    created RichText widget containing text and widget

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,45 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
+  final String str = 'Some really long text to show the working of a wrap widget'
+      + ' and place a button at the end of the text and it will then prove that'
+      + ' you can do the same with other kind of widgets.';
   @override
   Widget build(BuildContext context) {
-    // var pixelRatio = MediaQuery.of(context).devicePixelRatio;
     return Scaffold(
       body: SafeArea(
-        child: Container(
-
+        child: Center(
+          child: Container(
+            width: (MediaQuery.of(context).size.width) / 1.3,
+            decoration: BoxDecoration(
+              color: Colors.orange[100],
+            ),
+            child: RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: str,
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.black,
+                    ),
+                  ),
+                  WidgetSpan(
+                    child: SizedBox(width: 5,),
+                  ),
+                  WidgetSpan(
+                    child: Container(
+                      child: RaisedButton(
+                        color: Colors.blueAccent,
+                        onPressed: (){},
+                        child: Text('Button'),
+                      ),
+                    ),
+                  ),
+                ]
+              ),
+            ),
+          ),
         ),
       ),
     );
