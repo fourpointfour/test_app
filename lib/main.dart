c6378bf7445c073624aca09926ee69ed0601fdea
Vaibhav Kushwaha <vaibhav20092000@gmail.com>
2021-03-23 17:15:09 +0530

    added the colored shader, BlendMode.clear issue still unresolved

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main()
@@ -16,12 +17,43 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    // var pixelRatio = MediaQuery.of(context).devicePixelRatio;
+    final w = MediaQuery.of(context).size.width;
+    final h = MediaQuery.of(context).size.height;
     return Scaffold(
       body: SafeArea(
         child: Container(
+          child: ShaderMask(
+            child: Image.asset('lib/assets/bmi_triangle.png'),
+            shaderCallback: (bounds) {
+              return LinearGradient(
+                colors: [Colors.transparent, Colors.red],
+                begin: Alignment(0, 0),
+                end: Alignment.topCenter,
+              ).createShader(bounds);
+            },
+            blendMode: BlendMode.colorBurn,
+          )
         ),
       ),
     );
+  }
+}
+
+class MyPainter extends CustomPainter {
+  final pt = Paint()
+  ..color = Colors.black
+  ..strokeWidth = 10
+  ..strokeCap = StrokeCap.square;
+  @override
+  void paint(Canvas canvas, Size size)
+  {
+    final pt1 = Offset(20, 10), pt2 = Offset(size.width - 20, 10);
+    canvas.drawLine(pt1, pt2, pt);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter old)
+  {
+    return false;
   }
 }
